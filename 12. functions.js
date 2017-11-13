6da8c1be20d48469c5faafe979a5a301639261c3
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-11-13 16:16:28 +0700

    wtf js

--- 12. functions.js
+++ 12. functions.js
@@ -33,8 +33,18 @@ console.log(test());
 var arr = [];
 var nums = [1, 5, 10, 20];
 
-for (var i; i < nums.length; i++) {
+for (var i = 0; i < nums.length; i++) {
     arr.push(nums[i] * 2);
 }
 
 console.log(arr);
+
+var double = function(x) {
+    return 2 * x;
+};
+
+// var doubled = nums.map(double)
+var doubled = nums.map(function(x) {
+    return 2 * x;
+});
+console.log(doubled);
