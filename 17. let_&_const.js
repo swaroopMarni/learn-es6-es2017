f1c1e4c2ec9ad7d6e34b4967eb1b164f7a746ca7
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-11-14 09:56:20 +0700

    let & const

---
+++ 17. let_&_const.js
@@ -0,0 +1,35 @@
+let x = 1;
+var y = 1;
+
+{
+    let x = 2;
+    var y = 4;
+    console.log(x);
+    console.log(y);
+}
+
+console.log(x);
+console.log(y);
+
+// function sayMeow() {
+//     console.log(cat);
+//     console.log(dog);
+
+//     var cat = "meow";
+//     let dog = "dog";
+// }
+
+// sayMeow();
+
+// const cat1 = "meow";
+// cat1 = "m";
+// console.log("a");
+
+// const cat = {
+//     sound: "meow",
+// };
+
+// console.log(cat);
+
+// cat.sound = "woof";
+// console.log(cat);
