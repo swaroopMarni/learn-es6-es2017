02ccc976e0cd7e72e3a4ec685ddc8bc3eeed4b5a
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-11-18 21:08:06 +0700

    spread & rest operator

---
+++ 31. spread & rest.js
@@ -0,0 +1,11 @@
+// var middle = [3,4]
+// var arr = [1,2, ...middle, 5,6]
+
+// console.log(arr)
+
+var arr = [1, 2, 3];
+
+var arr2 = [...arr];
+arr2.push(4);
+console.log(arr);
+console.log(arr2);
